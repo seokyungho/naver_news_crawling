acc84c32260ddeaacab2b58af6d93a8fbdfda6bf
seokyungho <rudgh31115@naver.com>
2023-01-10 21:05:57 +0900

    수정

--- naver.py
+++ naver.py
@@ -1,8 +1,16 @@
 import requests
-from bs4 import Beautifulsoup
+from bs4 import BeautifulSoup #Beutifulsoup 의 s는 대문자여야함
 
-response = requests.get("http://www.naver.com")
+
+#header = {'User-agent' : 'Mozila/2.0'}
+#naver 서버에 대화 시도
+response = requests.get("https://sports.news.naver.com/index")
+
+
 html = response.text
-soup = Beautifulsoup(html,'html.parser')
-word = soup.select_one("#NM_set_home_btn")
-print(html)
+soup = BeautifulSoup(html,'html.parser')
+
+
+titles = soup.select(".link_today") #id는 맨앞에 # 붙혀야함
+for title in titles:
+    print(title.text.strip())
